f34083b436c206956080cbf0041d2d9e817ec1b5
valeriy <vlrp512@gmail.com>
2026-02-22 20:43:18 +0500

    Add auth viewModel

--- AndroidApp/app/src/main/java/app/xl/androidapp/presentation/auth/AuthFragment.kt
+++ AndroidApp/app/src/main/java/app/xl/androidapp/presentation/auth/AuthFragment.kt
@@ -1,31 +1,115 @@
 package app.xl.androidapp.presentation.auth
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import app.xl.androidapp.R
+import app.xl.androidapp.databinding.FragmentAuthBinding
+import kotlinx.coroutines.launch
+import kotlin.getValue
+import androidx.core.widget.doAfterTextChanged
+import com.google.android.material.internal.ViewUtils.showKeyboard
 
 class AuthFragment : Fragment() {
 
+    private var _binding: FragmentAuthBinding? = null
+    private val binding
+        get() = _binding ?: error("Binding is only valid between onCreateView and onDestroyView")
+
+    private val viewModel: AuthViewModel by viewModels()
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bindToViewModel()
+    }
 
-        val signInButton = view.findViewById<Button>(R.id.signInButton)
-        val navGraph = findNavController()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
-        signInButton.setOnClickListener {
-            navGraph.navigate(R.id.repositoriesListFragment)
+    private fun bindToViewModel() {
+        bindAuthState()
+        bindActions()
+        bindSignInButton()
+        bindInputs()
+    }
+
+    private fun bindAuthState() {
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                AuthViewModel.State.Idle -> {
+                    binding.signInButton.isEnabled = true
+                    binding.signInButton.setTextColor(Color.WHITE)
+                    binding.progressIndicator.hide()
+                }
+
+                AuthViewModel.State.Loading -> {
+                    binding.signInButton.isEnabled = false
+                    binding.signInButton.setTextColor(Color.TRANSPARENT)
+                    binding.progressIndicator.show()
+                }
+
+                is AuthViewModel.State.InvalidInput -> {
+                    binding.tokenInputLayout.error = state.reason
+                    binding.signInButton.setTextColor(Color.WHITE)
+                    binding.signInButton.isEnabled = true
+                    binding.progressIndicator.hide()
+                }
+            }
+        }
+    }
+
+    private fun bindActions() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.actions.collect { action ->
+                    when (action) {
+                        is AuthViewModel.Action.RouteToMain -> {
+                             findNavController().navigate(
+                                 R.id.action_authFragment_to_repositoriesListFragment
+                             )
+                        }
+                        is AuthViewModel.Action.ShowError -> {
+                            val message = action.message
+                            binding.tokenInputLayout.error = message
+                        }
+                        is AuthViewModel.Action.FocusOnTokenField -> {
+                            binding.tokenInputEdit.requestFocus()
+                            showKeyboard(binding.tokenInputEdit)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private fun bindSignInButton() {
+        binding.signInButton.setOnClickListener {
+            viewModel.onSignInButtonPressed()
+        }
+    }
+
+    private fun bindInputs() {
+        binding.tokenInputEdit.doAfterTextChanged { text ->
+            viewModel.onTokenChanged(text?.toString().orEmpty())
+            binding.tokenInputLayout.error = null
         }
     }
 }
---
+++ AndroidApp/app/src/main/java/app/xl/androidapp/presentation/auth/AuthViewModel.kt
@@ -0,0 +1,50 @@
+package app.xl.androidapp.presentation.auth
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+
+class AuthViewModel : ViewModel() {
+
+    private val _token = MutableLiveData<String>("")
+    val token: LiveData<String> = _token
+
+    private val _state = MutableLiveData<State>(State.Idle)
+    val state: LiveData<State> = _state
+
+    private val _actions = MutableSharedFlow<Action>()
+    val actions: Flow<Action> = _actions
+
+    fun onTokenChanged(text: String) {
+        _token.value = text
+    }
+
+    fun onSignInButtonPressed() {
+        val tokenValue = _token.value.orEmpty()
+
+        if (tokenValue.isBlank()) {
+            viewModelScope.launch {
+                _actions.emit(Action.FocusOnTokenField)
+            }
+            return
+        }
+
+        // TODO: Sign In request
+    }
+
+    sealed interface State {
+        object Idle : State
+        object Loading : State
+        data class InvalidInput(val reason: String) : State
+    }
+
+    sealed interface Action {
+        data class ShowError(val message: String) : Action
+        object RouteToMain : Action
+        object FocusOnTokenField : Action
+    }
+}
