4437135593fc8f809a1137a98a0000d2683f8885
valeriy <vlrp512@gmail.com>
2026-02-23 21:07:18 +0500

    Add RepositoriesListViewModel

--- AndroidApp/app/src/main/java/app/xl/androidapp/presentation/repositories/RepositoriesListFragment.kt
+++ AndroidApp/app/src/main/java/app/xl/androidapp/presentation/repositories/RepositoriesListFragment.kt
@@ -5,14 +5,60 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import app.xl.androidapp.R
+import androidx.fragment.app.viewModels
+import app.xl.androidapp.databinding.FragmentRepositoriesListBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RepositoriesListFragment : Fragment() {
+
+    private var _binding: FragmentRepositoriesListBinding? = null
+    private val binding
+        get() = _binding ?: error("Binding is only valid between onCreateView and onDestroyView")
+
+    private val viewModel: RepositoriesListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_repositories_list, container, false)
+        _binding = FragmentRepositoriesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindToViewModel()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun bindToViewModel() {
+        bindRepositoriesListState()
+    }
+
+    private fun bindRepositoriesListState() {
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                RepositoriesListViewModel.State.Empty -> {
+                    // TODO: - show empty placeholder
+                }
+
+                RepositoriesListViewModel.State.Loading -> {
+                    // TODO: - show loader
+                }
+
+                is RepositoriesListViewModel.State.Loaded -> {
+                    // TODO: - show content
+                }
+
+                is RepositoriesListViewModel.State.Error -> {
+                    // TODO: - show error placeholder
+                }
+            }
+        }
     }
 }
---
+++ AndroidApp/app/src/main/java/app/xl/androidapp/presentation/repositories/RepositoriesListViewModel.kt
@@ -0,0 +1,55 @@
+package app.xl.androidapp.presentation.repositories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.xl.androidapp.data.repository.toEntity
+import app.xl.androidapp.domain.entity.AppError
+import app.xl.androidapp.domain.entity.Repo
+import app.xl.androidapp.domain.repository.AppRepositoryInterface
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class RepositoriesListViewModel@Inject constructor(
+    private val repository: AppRepositoryInterface
+) : ViewModel() {
+
+    private val _state = MutableLiveData<State>(State.Loading)
+    val state: LiveData<State> = _state
+
+    init {
+        loadRepositories()
+    }
+
+    private fun loadRepositories() {
+        viewModelScope.launch {
+            _state.value = State.Loading
+            try {
+                val repositories = repository.getRepositories()
+
+                _state.value = if (repositories.isEmpty()) {
+                    State.Empty
+                } else {
+                    State.Loaded(repositories.map { it.toEntity() })
+                }
+            } catch (error: AppError) {
+                _state.value = State.Error(error.message.toString())
+                handleError(error)
+            }
+        }
+    }
+
+    private fun handleError(error: AppError) {
+        // TODO: - fill error placeholder
+    }
+
+    sealed interface State {
+        object Loading : State
+        data class Loaded(val repos: List<Repo>) : State
+        data class Error(val error: String) : State
+        object Empty : State
+    }
+}
